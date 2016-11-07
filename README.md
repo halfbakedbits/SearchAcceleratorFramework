fc271a3824aa506c4cdb6630b61d3734c7dc4d02
Brett Veenstra <brettveenstra@gmail.com>
2016-11-07 16:43:22 -0500

    changes to streamline nuget deployment

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SearchAcceleratorFramework
-Provides flexible Search framework as an easy alternative to Lucene and other fuller capable libraries.
+Provides flexible Search framework as an easy alternative to Lucene and other fuller capable libraries.  A framework to allow distributed and multi-field searches, without additional third-party libraries.  Is NOT a replacement to Lucene.
--- src/SearchAcceleratorFramework/Properties/AssemblyInfo.cs
+++ src/SearchAcceleratorFramework/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SearchAcceleratorFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides flexible Search framework as an easy alternative to Lucene and other fuller capable libraries.  A framework to allow distributed and multi-field searches, without additional third-party libraries.  Is NOT a replacement to Lucene.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Brett Veenstra")]
 [assembly: AssemblyProduct("SearchAcceleratorFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
